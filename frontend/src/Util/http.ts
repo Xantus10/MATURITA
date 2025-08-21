d165e1fd5c909161160d8e4b6be67b4cd8f0fdc7
Jaroslav Zaba <jaroslav.zaba@post.cz>
2025-08-21 15:33:51 +0200

    HTTP request wrapper

---
+++ frontend/src/Util/http.ts
@@ -0,0 +1,34 @@
+const BASE_URL = import.meta.env.VITE_BE_URL;
+
+function constructURL(path: string, args: object = {}) {
+  path = (path.length > 0) ? path : '/';
+  path = (path[0] === '/') ? path : '/'+path;
+  let url = new URL(`${BASE_URL}${path}`);
+  if (args && typeof args === 'object') {
+    Object.entries(args).forEach((val: string[]) => {
+      let k = val[0], v = val[1];
+      url.searchParams.append(k, v);
+    });
+  }
+  return url.toString();
+}
+
+export async function get(path: string, args: {[key: string]: string} = {}) {
+  let url = constructURL(path, args);
+  try {
+    let res = await fetch(url, {method: 'GET', headers: {'Accept': 'application/json'}});
+    return res;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function post(path: string, args: {[key: string]: string} = {}) {
+  let url = constructURL(path);
+  try {
+    let res = await fetch(url, {method: 'POST', headers: {'Accept': 'application/json', 'Content-Type': 'application/json'}, body: JSON.stringify(args)});
+    return res;
+  } catch (error) {
+    console.error(error);
+  }
+}
