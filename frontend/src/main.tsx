79d894b3aaab14f7b5a65223605b625758b04e52
Jaroslav Zaba <jaroslav.zaba@post.cz>
2025-08-21 15:35:22 +0200

    Basic providers setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,50 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+
+import { MantineProvider, createTheme } from '@mantine/core'
+import { Notifications } from '@mantine/notifications'
+
+import { MsalProvider } from "@azure/msal-react";
+import { type Configuration,  PublicClientApplication } from "@azure/msal-browser";
+
+// Mantine styles imports
+import '@mantine/core/styles.css'
+import '@mantine/dates/styles.css'
+import '@mantine/notifications/styles.css'
+
+import './main.css'
+
 import App from './App.tsx'
+
+// Custom global styles overrides
+const themeOverride = createTheme({
+  fontFamily: 'system-ui, Avenir, Helvetica, Arial, sans-serif',
+  autoContrast: true,
+  cursorType: 'pointer',
+});
+
+
+// MSAL configuration
+const configuration: Configuration = {
+    auth: {
+        clientId: "ab9faf06-dbf6-464e-86ac-8143396d920d",
+        authority: "https://login.microsoftonline.com/986a3974-67bd-4338-af77-835ff0f8b550"
+    }
+};
+
+const pca = new PublicClientApplication(configuration);
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <MsalProvider instance={pca}>
+      <MantineProvider theme={themeOverride} defaultColorScheme='dark'>
+        <BrowserRouter>
+          <App />
+          <Notifications />
+        </BrowserRouter>
+      </MantineProvider>
+    </MsalProvider>
   </StrictMode>,
 )
