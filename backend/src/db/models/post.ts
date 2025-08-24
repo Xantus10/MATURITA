58319b518ed0250c133fb2686bebc7d7108f7bcd
Jaroslav Zaba <jaroslav.zaba@post.cz>
2025-08-24 17:07:42 +0200

    Post create/delete

--- backend/src/db/models/post.ts
+++ backend/src/db/models/post.ts
@@ -8,7 +8,7 @@ export const postSchema = new Schema<PostIF, Model<PostIF>, PostModelIF>({
   CreatorId: {type: Schema.Types.ObjectId, ref: 'User', required: true},
   Title: {type: String, required: true},
   CreatedAt: {type: Date, default: Date.now},
-  RemoveAt: {type: Date, required: true, expires: 1},
+  RemoveAt: {type: Date, required: true, expires: 1, set: (days: number) => { return new Date(Date.now() + 86400 * 1000 * days) }},
   Subjects: {type: [String], validate: (v: string[]) => {return v.length > 0}},
   State: {type: String, enum: ['Like new', 'Good', 'Worn'], required: true},
   Years: {type: [Number], validate: (v: number[]) => {return v.length > 0}},
--- backend/src/routes/posts.ts
+++ backend/src/routes/posts.ts
@@ -8,19 +8,19 @@ const postsrouter = Router();
 postsrouter.use(loggedin);
 
 postsrouter.get('/', async (req: Request, res: Response) => {
-  if (!req.params.begin) {
+  if (!req.query.begin) {
     return res.status(400).send({msg: "'begin' parameter missing"});
   }
-  if (!req.params.orderBy) {
+  if (!req.query.orderBy) {
     return res.status(400).send({msg: "'orderBy' parameter missing"});
   }
-  let begin = parseInt(req.params.begin);
-  let {orderBy, filterSubjects, filterYears, filterState, priceMin, priceMax} = req.params;
+  let begin = parseInt(req.query.begin as string);
+  let {orderBy, filterSubjects, filterYears, filterState, priceMin, priceMax} = req.query;
   if (!(filterState && filterSubjects && filterYears)) {
     return res.status(400).send({msg: "'filter*' parameter/s missing"});
   }
-  let dbmin = (priceMin) ? parseInt(priceMin) : MIN_RANGE;
-  let dbmax = (priceMax) ? parseInt(priceMax) : MAX_RANGE;
+  let dbmin = (priceMin) ? parseInt(priceMin as string) : MIN_RANGE;
+  let dbmax = (priceMax) ? parseInt(priceMax as string) : MAX_RANGE;
   let dbsort: {[key: string]: 1 | -1} = (orderBy === 'price') ? { 'Price.Min': 1 } : { 'CreatedAt': -1 };
 
   let posts = await Post.find({ Subjects: { $in: filterSubjects }, Years: { $in: filterYears }, State: { $in: filterState }, 'Price.Min': { $lte: dbmax }, 'Price.Max': { $gte: dbmin } })
@@ -32,11 +32,33 @@ postsrouter.get('/', async (req: Request, res: Response) => {
 });
 
 postsrouter.post('/', async (req: Request, res: Response) => {
-  
+  if (req.session.data?.role !== 'user') {
+    return res.status(403).send({msg: "Unauthorized (not user)!"});
+  }
+  let {title, remove, subjects, state, years, priceMin, priceMax} = req.body;
+  if (!(title && remove && subjects && state && years && priceMin)) {
+    return res.status(400).send({msg: "Required parameter/s missing"});
+  }
+  let min = parseInt(priceMin);
+  let max = (priceMax) ? parseInt(priceMax) : min;
+  let yearsArr = years.map(parseInt);
+  await Post.create({ CreatorId: req.session.data?.objId, Title: title, RemoveAt: parseInt(remove), Subjects: subjects, State: state, Years: yearsArr, Price: { Min: min, Max: max } });
+  return res.status(201).send({msg: "Post created"});
 });
 
 postsrouter.delete('/', async (req: Request, res: Response) => {
-  
+  if (!req.body.postId) {
+    return res.status(400).send({msg: "'postId' parameter/s missing"});
+  }
+  let postId = new Types.ObjectId(req.body.postId as string);
+  let post = await Post.findById(postId);
+  if (!post) {
+    return res.status(404).send({msg: "Post does not exist!"});
+  }
+  if (!(req.session.data?.role === 'admin' || req.session.data?.objId === post._id)) {
+    return res.status(403).send({msg: "Unauthorized (not admin nor creator)!"});
+  }
+  await post.deleteOne();
 });
 
 
