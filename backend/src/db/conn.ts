4953b5c8e504a19f51b5afe7a594e042fa1f8f3e
Jaroslav Zaba <jaroslav.zaba@post.cz>
2025-08-26 17:40:04 +0200

    Disable buffering

--- backend/src/db/conn.ts
+++ backend/src/db/conn.ts
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
 import env from "../config/envconfig.js";
 
+
+mongoose.set('bufferCommands', false);
+
 export default async function connect() {
   try {
     await mongoose.connect(env.DBURI);
