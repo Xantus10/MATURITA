b87addf6f36820a11384640d8b1882a05aba9ca0
Jaroslav Zaba <jaroslav.zaba@post.cz>
2025-08-30 16:26:46 +0200

    Moved RegEx check to body bcs of express 5

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -11,9 +11,10 @@ usersrouter.get('/', async (req: Request, res: Response) => {
   
 });
 
-usersrouter.get('/:id([0-9a-fA-F]{24})', async (req: Request, res: Response) => {
+usersrouter.get('/:id', async (req: Request, res: Response) => {
   let id = req.params.id;
   if (!id) return res.status(404).send({msg: 'The user does not exist'});
+  if (!(/[0-9a-fA-F]{24}/.test(id))) return res.status(400).send({msg: 'The id is not valid mongodb id'});
   let doc = await User.findById(new Types.ObjectId(id), { Name: 1 });
   if (!doc) return res.status(404).send({msg: 'The user does not exist'});
   return doc;
