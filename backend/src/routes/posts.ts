ee3693580d30248526155b4b95df2ab5c275487e
Jaroslav Zaba <jaroslav.zaba@post.cz>
2025-09-02 15:49:59 +0200

    Added /extend and /user routes

--- backend/src/routes/posts.ts
+++ backend/src/routes/posts.ts
@@ -1,5 +1,5 @@
 import { Router, type Request, type Response } from "express";
-import { loggedin } from "../middlewares/session.js";
+import { checkRole, loggedin } from "../middlewares/session.js";
 import { Types } from "mongoose";
 import Post, { MIN_RANGE, MAX_RANGE } from "../db/models/post.js";
 import { strQueryToArray } from "../util/parse.js";
@@ -69,10 +69,7 @@ const multerMiddleware = multer({
   limits: { fileSize: 5 * 1024 * 1024 }
 });
 
-postsrouter.post('/', multerMiddleware.array('pictures', 3), async (req: Request, res: Response) => {
-  if (req.session.data?.role !== 'user') {
-    return res.status(403).send({msg: "Unauthorized (not user)!"});
-  }
+postsrouter.post('/', checkRole('user'), multerMiddleware.array('pictures', 3), async (req: Request, res: Response) => {
   let {title, remove, subjects, state, years, priceMin, priceMax} = req.body;
   if (!(title && remove && subjects && state && years && priceMin)) {
     return res.status(400).send({msg: "Required parameter/s missing"});
@@ -95,11 +92,42 @@ postsrouter.delete('/', async (req: Request, res: Response) => {
   if (!post) {
     return res.status(404).send({msg: "Post does not exist!"});
   }
-  if (!(req.session.data?.role === 'admin' || req.session.data?.objId === post._id)) {
+  if (!(req.session.data?.role === 'admin' || req.session.data?.objId === post.CreatorId)) {
     return res.status(403).send({msg: "Unauthorized (not admin nor creator)!"});
   }
   await post.deleteOne();
 });
 
+postsrouter.post('/extend', checkRole('user'), async (req: Request, res: Response) => {
+  if (!req.body.postId) {
+    return res.status(400).send({msg: "'postId' parameter/s missing"});
+  }
+  if (!req.body.days) {
+    return res.status(400).send({msg: "'days' parameter/s missing"});
+  }
+  let postId = new Types.ObjectId(req.body.postId as string);
+  let days = parseInt(req.body.days);
+  if (days > 30) {
+    return res.status(400).send({msg: "You cannot extend more than 30 days"});
+  }
+  let post = await Post.findById(postId);
+  if (!post) {
+    return res.status(404).send({msg: "Post does not exist!"});
+  }
+  if (post.CreatorId !== req.session.data?.objId) {
+    return res.status(403).send({msg: "Unauthorized (not creator)!"});
+  }
+  Post.extendRemoveAt(postId, days);
+  return res.status(200).send({msg: `Posts lifetime has been extended by ${days} days`});
+});
+
+postsrouter.get('/user', async (req: Request, res: Response) => {
+  if (req.query.userId && req.session.data?.role !== "admin") {
+    return res.status(403).send({msg: "Unauthorized (not admin)!"});
+  }
+  let userId = (req.query.userId) ? new Types.ObjectId(req.query.userId as string) : req.session.data?.objId;
+  let posts = await Post.find({ CreatorId: userId });
+  return res.status(200).send({posts: posts});
+});
 
 export default postsrouter;
