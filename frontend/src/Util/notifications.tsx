9379282fb3f9808394d4ac9055bd0cf3a7e1a109
Jaroslav Zaba <jaroslav.zaba@post.cz>
2025-09-11 17:52:43 +0200

    Auto HTTP notification

--- frontend/src/Util/notifications.tsx
+++ frontend/src/Util/notifications.tsx
@@ -2,7 +2,13 @@ import { notifications } from "@mantine/notifications";
 import { FaExclamationTriangle, FaInfoCircle, FaCheckCircle } from "react-icons/fa";
 
 
-export function showNotification(title: string, message: string="", icon: 'OK' | 'INFO' | 'ERR'='OK') {
+export interface showNotificationProps {
+  title: string;
+  message?: string;
+  icon?: 'OK' | 'INFO' | 'ERR';
+};
+
+export function showNotification({ title, message="", icon='OK' }: showNotificationProps) {
   notifications.show({
     title: title,
     message: message,
@@ -10,4 +16,20 @@ export function showNotification(title: string, message: string="", icon: 'OK' |
     autoClose: 5000,
     icon: (icon === 'OK') ? <FaCheckCircle /> : ((icon === 'INFO') ? <FaInfoCircle /> : <FaExclamationTriangle />),
   });
+}
+
+export async function autoHttpResponseNotification(res: Response, with200: boolean=false) {
+  let status
+  let msg;
+  if (res.status) {
+    status = res.status;
+    let js = await res.json()
+    msg = js.msg;
+  } else {
+    status = 500;
+    msg = "FE res undefined!"
+  }
+  if (status === 200 && !with200) return;
+  let props: showNotificationProps = {title: msg, icon: (status in [200, 201]) ? 'OK' : 'ERR'};
+  showNotification(props);
 }
