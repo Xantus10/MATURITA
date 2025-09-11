06c85a2184a615b2baa7a59227cfbef951b773bf
Jaroslav Zaba <jaroslav.zaba@post.cz>
2025-09-11 18:02:05 +0200

    Delete route 200 res added

--- backend/src/routes/posts.ts
+++ backend/src/routes/posts.ts
@@ -96,6 +96,7 @@ postsrouter.delete('/', async (req: Request, res: Response) => {
     return res.status(403).send({msg: "Unauthorized (not admin nor creator)!"});
   }
   await post.deleteOne();
+  return res.status(200).send({msg: 'Post deleted'});
 });
 
 postsrouter.post('/extend', checkRole('user'), async (req: Request, res: Response) => {
