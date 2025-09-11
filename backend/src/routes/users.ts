5bc491ad32ea00c5c70a1fa1b7501f11897b67a9
Xantus10 <jaroslav.zaba@post.cz>
2025-09-11 10:13:34 +0200

    BE users routes completed

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,5 +1,5 @@
 import { Router, type Request, type Response } from "express";
-import { loggedin } from "../middlewares/session.js";
+import { loggedin, checkRole } from "../middlewares/session.js";
 import { Types } from "mongoose";
 import User from "../db/models/user.js";
 
@@ -7,9 +7,27 @@ const usersrouter = Router();
 
 usersrouter.use(loggedin);
 
-usersrouter.get('/list', async (req: Request, res: Response) => {
-  
+usersrouter.get('/list', checkRole('admin'), async (req: Request, res: Response) => {
+  let first = (req.query.first) ? req.query.first : "";
+  let last = (req.query.last) ? req.query.last : "";
+  let limit = (req.query.limit) ? parseInt(req.query.limit as string) : 20;
+  let docs = await User.find({ "Name.First": { $regex: `^${first}.*$`, $options: 'i' }, "Name.Last": { $regex: `^${last}.*$`, $options: 'i' } }).limit(limit);
+  return res.status(200).send({users: docs});
 });
+
+
+usersrouter.get('/me', async (req: Request, res: Response) => {
+  let id = new Types.ObjectId(req.session.data?.objId);
+  let doc = await User.findById(id, { Name: 1 });
+  return res.status(200).send(doc);
+});
+
+usersrouter.delete('/me', async (req: Request, res: Response) => {
+  let id = new Types.ObjectId(req.session.data?.objId);
+  await User.findByIdAndDelete(id);
+  return res.status(200).send({msg: 'OK'});
+});
+
 
 usersrouter.get('/:id', async (req: Request, res: Response) => {
   let id = req.params.id;
@@ -18,6 +36,14 @@ usersrouter.get('/:id', async (req: Request, res: Response) => {
   let doc = await User.findById(new Types.ObjectId(id), { Name: 1 });
   if (!doc) return res.status(404).send({msg: 'The user does not exist'});
   return res.status(200).send(doc);
+});
+
+usersrouter.delete('/:id', checkRole('admin'), async (req: Request, res: Response) => {
+  let id = req.params.id;
+  if (!id) return res.status(404).send({msg: 'The user does not exist'});
+  if (!(/[0-9a-fA-F]{24}/.test(id))) return res.status(400).send({msg: 'The id is not valid mongodb id'});
+  await User.findByIdAndDelete(id);
+  return res.status(200).send({msg: 'OK'});
 });
 
 
