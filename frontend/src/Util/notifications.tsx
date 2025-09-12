523931e68bb42fe58d1dd74b1b83ac999583c86a
Jaroslav Zaba <jaroslav.zaba@post.cz>
2025-09-12 14:49:04 +0200

    res.clone + visual bugs

--- frontend/src/Util/notifications.tsx
+++ frontend/src/Util/notifications.tsx
@@ -9,11 +9,13 @@ export interface showNotificationProps {
 };
 
 export function showNotification({ title, message="", icon='OK' }: showNotificationProps) {
+  console.log(icon);
   notifications.show({
     title: title,
     message: message,
     withCloseButton: true,
     autoClose: 5000,
+    color: (icon === 'OK') ? "green" : ((icon === 'INFO') ? "blue" : "red"),
     icon: (icon === 'OK') ? <FaCheckCircle /> : ((icon === 'INFO') ? <FaInfoCircle /> : <FaExclamationTriangle />),
   });
 }
@@ -23,13 +25,14 @@ export async function autoHttpResponseNotification(res: Response, with200: boole
   let msg;
   if (res.status) {
     status = res.status;
-    let js = await res.json()
+    let js = await res.clone().json()
     msg = js.msg;
   } else {
     status = 500;
     msg = "FE res undefined!"
   }
   if (status === 200 && !with200) return;
+  console.log(status);
   let props: showNotificationProps = {title: msg, icon: (status in [200, 201]) ? 'OK' : 'ERR'};
   showNotification(props);
 }
