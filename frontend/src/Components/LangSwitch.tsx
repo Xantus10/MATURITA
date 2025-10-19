089f2d429d04bc9c96df7e09af17b79634eec313
Jaroslav Zaba <jaroslav.zaba@post.cz>
2025-10-19 14:42:09 +0200

    LangSwitch not showing more than fallbackLng bugfix

--- frontend/src/Components/LangSwitch.tsx
+++ frontend/src/Components/LangSwitch.tsx
@@ -1,13 +1,16 @@
 import { NativeSelect } from "@mantine/core";
 import i18next from "i18next";
 
+
+export const SUPPORTED_LANGUAGES = ['cs', 'en'];
+
 /**
  * Select to change language
  */
 function LangSwitch() {
   return (
     <>
-      <NativeSelect data={Object.keys(i18next.store.data)} value={i18next.language} onChange={(e) => i18next.changeLanguage(e.currentTarget.value)} />
+      <NativeSelect data={SUPPORTED_LANGUAGES} value={i18next.language} onChange={(e) => i18next.changeLanguage(e.currentTarget.value)} />
     </>
   );
 }
